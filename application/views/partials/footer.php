7d719541f9f6c09866b687a34206d34a583724e6
Afonso <afonsonetgomes@gmail.com>
2015-08-23 14:00:32 -0300

    Retira ação de enviar mensagem através do botão e adiciona ação ao apertar enter

--- application/views/partials/footer.php
+++ application/views/partials/footer.php
@@ -11,11 +11,8 @@
 		    	</div>
 		    	<div id="digitar">
 		    		<div class="row">
-		    			<div class="col-xs-8">
+		    			<div class="col-xs-12">
 		    				<input type="text" class="form-control" id="mensagem">
-		    			</div>
-		    			<div class="col-xs-4">
-		    				<button type="button" class="btn btn-primary btn-sm form-control" id="enviar-mensagem">Enviar</button>
 		    			</div>
 		    		</div>
 		    	</div>
