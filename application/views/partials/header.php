03c1e92e0142eec8b00d7fa914737b30b523ba47
eliseu <eliseu@medlynx.com.br>
2015-08-24 13:32:33 -0300

    Altera Titulo

--- application/views/partials/header.php
+++ application/views/partials/header.php
@@ -6,6 +6,6 @@
       <link rel="stylesheet" href="<?=base_url('assets/css/style.css?t=' . time());?>">
       <link rel="stylesheet" href="<?=base_url('assets/css/font-awesome.min.css?t=' . time());?>">
       <link rel="stylesheet" href="<?=base_url('assets/css/chat.css?t=' . time());?>">
-      <title>Local</title>
+      <title>Local Email</title>
     </head>
     <body>
